1b5a420a787e14b8df3575e59277646f02c52368
Regielberson Alexandre Pereira Lirio <rlirio@supriservice.local>
2022-05-20 16:56:14 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,17 +1,14 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "type/movie";
+
+type Props = {
+    movie : Movie;
+  }
 
 
-function MovieCard() {
 
-
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+function MovieCard( { movie } : Props) {
 
     return (
 
@@ -23,6 +20,7 @@ function MovieCard() {
         <Link to={`/form/${movie.id}`}>
 
         <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        
         </Link>
         
     </div>
